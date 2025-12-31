130f45d027b4abb89b68540a4f25d92634b7086f
yoann74540 <dezeuze@hotmail.fr>
2025-12-31 11:55:40 +0100

    Clean structure software

--- public/ui.js
+++ public/ui.js
@@ -20,3 +20,19 @@ function showError(message, duration) {
 function showWarning(message, duration) {
     showMessage("warning",message,duration);
 }
+
+function showHelp() {
+  document.getElementById('help-modal').style.display = 'block';
+
+  //active le bouton i
+  document.getElementById('helpBtn').classList.add('active');
+  document.getElementById('homeBtn').classList.remove('active');
+}
+
+function showHome() {
+  document.getElementById('help-modal').style.display = 'none';
+
+  //active le bouton home
+  document.getElementById('homeBtn').classList.add('active');
+  document.getElementById('helpBtn').classList.remove('active');
+}
